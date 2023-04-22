f4c6875979c6437c57633f565ad4e05009ca6331
AJIT SHARMA <rohanrocky93@gmail.com>
2023-04-22 17:23:31 +0530

    including some member fun. of list 'l'.

--- list_stl.cpp
+++ list_stl.cpp
@@ -7,6 +7,15 @@ int main()
     cout<<endl<<"   "<<l.front();
     cout<<endl<<endl<<"   "<<l.back();
     cout<<endl<<endl;
+    l.push_front(2);
+    l.push_back(88);
+
+    l.emplace_back(55);
+    l.emplace_front(1);
+
+    l.pop_front();
+    l.pop_back();
+
     for(auto t= l.begin();t!= l.end();t++)
         cout<<"   "<<*t;
     cout<<endl;
